5c8cb38f0dc671e50fd07a5d948f1786084c4615
cherry-SC <sc_1226@163.com>
2026-02-04 13:13:13 +0800

    组件库-ScSelect组件初始化

---
+++ src/components/Select/Select.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sc-select"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ScSelect",
+  // inheritAttrs: false,
+});
+</script>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
